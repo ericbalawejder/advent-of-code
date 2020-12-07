0bda5c5e077832b14592a3c6b4a7535186ce23fd
Eric Balawejder <eric.balawejder@gmail.com>
2020-12-07 01:04:08 -0500

    day6 part1

--- src/main/java/aoc/day6/CustomCustoms.java
+++ src/main/java/aoc/day6/CustomCustoms.java
@@ -3,7 +3,6 @@ package aoc.day6;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
@@ -12,11 +11,10 @@ import java.util.stream.Collectors;
 public class CustomCustoms {
 
     public static void main(String[] args) throws IOException {
-        String[][] data = readFromFile("src/main/java/aoc/day6/test.txt");
-        System.out.println(Arrays.deepToString(data));
-        System.out.println(countAnswers2(data));
-
-        System.out.println(sumOfCounts(countAnswers2(data)));
+        String[][] data = readFromFilePart1("src/main/java/aoc/day6/test.txt");
+        //System.out.println(Arrays.deepToString(data));
+        //System.out.println(countAnswers(data));
+        System.out.println(sumOfCounts(countAnswers(data)));
     }
 
     static int sumOfCounts(List<Set<Character>> counts) {
@@ -26,18 +24,6 @@ public class CustomCustoms {
     }
 
     static List<Set<Character>> countAnswers(String[][] groupAnswers) {
-        final List<Set<Character>> uniqueAnswers = new ArrayList<>();
-        for (String[] group : groupAnswers) {
-            Set<Character> yes = Arrays.stream(group)
-                    .flatMap(s -> s.chars().mapToObj(c -> (char) c))
-                    .collect(Collectors.toUnmodifiableSet());
-
-            uniqueAnswers.add(yes);
-        }
-        return List.copyOf(uniqueAnswers);
-    }
-
-    static List<Set<Character>> countAnswers2(String[][] groupAnswers) {
         return Arrays.stream(groupAnswers)
                 .map(array -> Arrays.stream(array)
                     .flatMap(s -> s.chars().mapToObj(c -> (char) c))
@@ -45,7 +31,7 @@ public class CustomCustoms {
                 .collect(Collectors.toUnmodifiableList());
     }
 
-    private static String[][] readFromFile(String path) throws IOException {
+    private static String[][] readFromFilePart1(String path) throws IOException {
         final String data = Files.readString(Path.of(path));
         return Arrays.stream(data.split("\n\n"))
                 .map(s -> s.replace("\n", ""))

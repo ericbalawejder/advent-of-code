da5bf601a31ad285dd7ca3cc6c51f19ee48890f8
Eric Balawejder <eric.balawejder@gmail.com>
2020-12-08 01:27:54 -0500

    Created binary conversion for day5

--- src/main/java/aoc/day5/BinaryBoarding.java
+++ src/main/java/aoc/day5/BinaryBoarding.java
@@ -3,76 +3,42 @@ package aoc.day5;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class BinaryBoarding {
 
     public static void main(String[] args) {
-        final List<String> boardingPasses =
-                readFromFile("src/main/java/aoc/day5/boarding-passes.txt");
-        System.out.println(highestBoardingPassSeatId(boardingPasses));
-        System.out.println(findMySeatId(seatIds(boardingPasses)));
+        final SortedSet<Integer> boardingPasses =
+                readFromFileMapToTreeSet("src/main/java/aoc/day5/boarding-passes.txt");
+        System.out.println(boardingPasses.last());
+        System.out.println(findMySeat(boardingPasses));
     }
 
-    // Part 1
-    static int highestBoardingPassSeatId(List<String> boardingPasses) {
-        return seatIds(boardingPasses).stream()
-                .reduce(Integer::max)
+    static int findMySeat(SortedSet<Integer> boardingPasses) {
+        return IntStream.rangeClosed(boardingPasses.first(), boardingPasses.last())
+                .filter(n -> !boardingPasses.contains(n))
+                .findFirst()
                 .orElse(-1);
     }
 
-    // Part 2
-    static int findMySeatId(Set<Integer> seatIds) {
-        return seatIds.stream()
-                .filter(id -> seatIds.contains(id) &&
-                            !seatIds.contains(id + 1) &&
-                            seatIds.contains(id + 2))
-                .findAny()
-                .orElse(-1) + 1;
+    private static int encodingToSeatNumber(String seatEncoding) {
+        final String binary = seatEncoding
+                .replaceAll("[FL]", "0")
+                .replaceAll("[BR]", "1");
+        return Integer.parseInt(binary, 2);
     }
 
-    private static Set<Integer> seatIds(List<String> boardingPasses) {
-        return boardingPasses.stream()
-                .map(BinaryBoarding::calculateSeatId)
-                .collect(Collectors.toUnmodifiableSet());
-    }
-
-    private static int calculateSeatId(String seatEncoding) {
-        int rowLow = 0;
-        int rowHigh = 127;
-        int columnLow = 0;
-        int columnHigh = 7;
-        for (char c : seatEncoding.toCharArray()) {
-            switch (c) {
-                case 'F':
-                    rowHigh -= (rowHigh - rowLow + 1) / 2;
-                    break;
-                case 'B':
-                    rowLow += (rowHigh - rowLow + 1) / 2;
-                    break;
-                case 'L':
-                    columnHigh -= (columnHigh - columnLow + 1) / 2;
-                    break;
-                case 'R':
-                    columnLow += (columnHigh - columnLow + 1) / 2;
-                    break;
-                default:
-                    throw new IllegalArgumentException("Bad encoding");
-            }
-        }
-        return rowHigh * 8 + columnHigh;
-    }
-
-    private static List<String> readFromFile(String path) {
+    private static SortedSet<Integer> readFromFileMapToTreeSet(String path) {
         try (Stream<String> stream = Files.lines(Paths.get(path))) {
-            return stream.collect(Collectors.toCollection(ArrayList::new));
+            return stream.map(BinaryBoarding::encodingToSeatNumber)
+                    .collect(Collectors.toCollection(TreeSet::new));
         } catch (IOException e) {
             e.printStackTrace();
-            return new ArrayList<>();
+            return new TreeSet<>();
         }
     }
 }

4b239a13c0fd0897a7d3455445fadb24dde7cde2
Eric Balawejder <eric.balawejder@protonmail.com>
2021-11-17 23:56:16 -0500

    CrabCombat part 2

--- src/main/java/aoc/day22/CrabCombat.java
+++ src/main/java/aoc/day22/CrabCombat.java
@@ -3,12 +3,10 @@ package aoc.day22;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -19,82 +17,40 @@ import java.util.stream.Stream;
 public class CrabCombat {
 
     public static void main(String[] args) throws IOException {
-        final List<String> cards = readFile("src/main/java/aoc/day22/test.txt");
+        final List<String> cards = readFile("src/main/java/aoc/day22/deck.txt");
         final Map<String, List<Integer>> hands = dealHands(cards);
+
         final List<List<Integer>> completedGame = playCombat(hands);
         System.out.println(score(completedGame));
 
-        final List<List<Integer>> completedGames =
-                playRecursiveCombat(hands.get("Player1"), hands.get("Player2"));
+        final List<List<Integer>> completedGames = playRecursiveCombat(hands);
         System.out.println(score(completedGames));
-
-        final List<List<Integer>> completedGames2 =
-                playRecursiveCombat2(hands);
-        System.out.println(score(completedGames2));
     }
 
     static int score(List<List<Integer>> decks) {
         return decks.get(0).isEmpty() ?
-                calculateScore(decks.get(1)) :
-                calculateScore(decks.get(0));
+                calculateScore(decks.get(1)) : calculateScore(decks.get(0));
     }
 
-    static List<List<Integer>> playRecursiveCombat(List<Integer> player1, List<Integer> player2) {
-        final List<Integer> player1Deck = new ArrayList<>(player1);
-        final List<Integer> player2Deck = new ArrayList<>(player2);
-        final Set<Integer> decks = new HashSet<>();
-
-        while (!player1Deck.isEmpty() && !player2Deck.isEmpty()) {
-            final int deck = player1Deck.hashCode() * 31 + player2Deck.hashCode();
-            if (!decks.add(deck)) {
-                return List.of(player1Deck, new LinkedList<>());
-            }
-
-            final int player1Card = player1Deck.remove(0);
-            final int player2Card = player2Deck.remove(0);
-            if (player1Card <= player1Deck.size() && player2Card <= player2Deck.size()) {
-                final List<List<Integer>> resultDecks = playRecursiveCombat(
-                        player1Deck.subList(0, player1Card),
-                        player2Deck.subList(0, player2Card));
-
-                if (!resultDecks.get(0).isEmpty()) {
-                    player1Deck.add(player1Card);
-                    player1Deck.add(player2Card);
-                } else {
-                    player2Deck.add(player2Card);
-                    player2Deck.add(player1Card);
-                }
-            } else {
-                if (player1Card > player2Card) {
-                    player1Deck.add(player1Card);
-                    player1Deck.add(player2Card);
-                } else {
-                    player2Deck.add(player2Card);
-                    player2Deck.add(player1Card);
-                }
-            }
-        }
-        return List.of(player1Deck, player2Deck);
-    }
-
-    static List<List<Integer>> playRecursiveCombat2(Map<String, List<Integer>> handsByPlayer) {
+    static List<List<Integer>> playRecursiveCombat(Map<String, List<Integer>> handsByPlayer) {
         final Map<String, List<Integer>> hands = new HashMap<>(handsByPlayer);
         final Set<Integer> decks = new HashSet<>();
         while (!hands.get("Player1").isEmpty() && !hands.get("Player2").isEmpty()) {
             final int deck = hands.get("Player1").hashCode() * 31 + hands.get("Player2").hashCode();
-            if(!decks.add(deck)) {
+            if (!decks.add(deck)) {
                 return List.of(hands.get("Player1"), Collections.emptyList());
             }
             final Integer card1 = hands.get("Player1").get(0);
             final Integer card2 = hands.get("Player2").get(0);
             hands.put("Player1", subtractCard(hands.get("Player1")));
             hands.put("Player2", subtractCard(hands.get("Player2")));
-            if (card1 <= hands.get("Player1").size() &&
-                    card2 <= hands.get("Player2").size()) {
+            if (card1 <= hands.get("Player1").size() && card2 <= hands.get("Player2").size()) {
+
                 final Map<String, List<Integer>> recursiveGameHands = Map.of(
                         "Player1", hands.get("Player1").subList(0, card1),
                         "Player2", hands.get("Player2").subList(0, card2));
-                final List<List<Integer>> resultDecks = playRecursiveCombat2(recursiveGameHands);
+
+                final List<List<Integer>> resultDecks = playRecursiveCombat(recursiveGameHands);
 
                 if (!resultDecks.get(0).isEmpty()) {
                     hands.put("Player1", addCards(hands.get("Player1"), List.of(card1, card2)));
@@ -117,17 +73,15 @@ public class CrabCombat {
         while (!hands.get("Player1").isEmpty() && !hands.get("Player2").isEmpty()) {
             final Integer card1 = hands.get("Player1").get(0);
             final Integer card2 = hands.get("Player2").get(0);
+            hands.put("Player1", subtractCard(hands.get("Player1")));
+            hands.put("Player2", subtractCard(hands.get("Player2")));
             if (card1 > card2) {
                 hands.put("Player1", addCards(hands.get("Player1"), List.of(card1, card2)));
             } else {
                 hands.put("Player2", addCards(hands.get("Player2"), List.of(card2, card1)));
             }
-            hands.put("Player1", subtractCard(hands.get("Player1")));
-            hands.put("Player2", subtractCard(hands.get("Player2")));
         }
-        return hands.values()
-                .stream()
-                .collect(Collectors.toUnmodifiableList());
+        return List.of(hands.get("Player1"), hands.get("Player2"));
     }
 
     private static int calculateScore(List<Integer> deck) {

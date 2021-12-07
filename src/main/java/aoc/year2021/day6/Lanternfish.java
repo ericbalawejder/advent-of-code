30a4d4ebd78b40a0335ce7af970de64e1f3ee6fe
Eric Balawejder <eric.balawejder@protonmail.com>
2021-12-07 14:27:39 -0500

    2021 day6 part 2 wip

--- src/main/java/aoc/year2021/day6/Lanternfish.java
+++ src/main/java/aoc/year2021/day6/Lanternfish.java
@@ -5,43 +5,60 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Lanternfish {
 
+    private final static Map<Integer, Integer> FISH_AGES = IntStream.rangeClosed(0, 8)
+            .boxed()
+            .collect(Collectors.groupingBy(
+                    Function.identity(),
+                    Collectors.reducing(0, e -> 0, Integer::sum)));
+
     public static void main(String[] args) throws IOException {
-        final String path = "src/main/java/aoc/year2021/day6/lanternfish-ages.txt";
+        //final String path = "src/main/java/aoc/year2021/day6/lanternfish-ages.txt";
+        final String path = "src/main/java/aoc/year2021/day6/test.txt";
         final List<Integer> agesOfFish = getFish(path);
-        System.out.println(countFish(agesOfFish, 80));
     }
 
-    static long countFish(List<Integer> agesOfFish, int numberOfDays) {
-        List<Integer> fish = new ArrayList<>(agesOfFish);
-        for (int day = 0; day < numberOfDays; day++) {
-            final int zeros = (int) fish.stream()
-                    .filter(i -> i == 0)
-                    .count();
+    static long countLanternfish(List<Integer> agesOfFish, int numberOfDays) {
+        final Map<Integer, Integer> fishByNumber = agesOfFish.stream()
+                .collect(Collectors.groupingBy(
+                        Function.identity(),
+                        Collectors.reducing(0, e -> 1, Integer::sum)));
 
-            final List<Integer> resetAge = fish.stream()
-                    .map(Lanternfish::reproduce)
+        Map<Integer, Integer> fishCount = Stream.of(FISH_AGES, fishByNumber)
+                .flatMap(map -> map.entrySet().stream())
+                .collect(Collectors.collectingAndThen(
+                        Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, Integer::sum),
+                        Collections::unmodifiableMap));
+
+        for (int i = 0; i < numberOfDays; i++) {
+            final int zeros = fishCount.get(0);
+            System.out.println("zeros= " + zeros);
+            final List<Integer> ages = Stream.concat(
+                            nextDay(new ArrayList<>(fishCount.values())).stream(),
+                            IntStream.range(0, zeros).map(e -> 0).boxed())
                     .toList();
 
-            final List<Integer> addNewFish = Stream.concat(
-                            resetAge.stream(),
-                            IntStream.range(0, zeros).mapToObj(i -> 9))
-                    .toList();
-
-            fish = addNewFish.stream()
-                    .map(i -> i - 1)
-                    .toList();
+            System.out.println("ages " + ages);
         }
-        return fish.size();
+        System.out.println(fishCount);
+        return fishCount.values()
+                .stream()
+                .reduce(0, Math::addExact);
     }
 
-    private static int reproduce(int age) {
-        return age == 0 ? 7 : age;
+    private static List<Integer> nextDay(List<Integer> count) {
+        return count.stream()
+                .map(i -> i - 1)
+                .toList();
     }
 
     private static List<Integer> getFish(String path) throws IOException {

290e4020ec2f8a45d6ead894cc6cdd78f197d032
Eric Balawejder <eric.balawejder@protonmail.com>
2021-12-15 02:07:47 -0500

    Updated MapUtils with comments.

--- src/main/java/aoc/utils/MapUtils.java
+++ src/main/java/aoc/utils/MapUtils.java
@@ -7,7 +7,22 @@ import java.util.stream.Collectors;
 
 public class MapUtils {
 
-    // In case of one-to-one relationship, you can return the first matched key.
+    // In case of one-to-one relationship, we can return the first matched key.
+    public static <T, E> T getKeysByValueT(Map<T, E> map, E value) {
+        return map.entrySet()
+                .stream()
+                .filter(entry -> Objects.equals(entry.getValue(), value))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElseThrow();
+    }
+
+    /**
+     * Possible one-to-many K -|--<- V
+     * Returning empty collection > Optional because if the value is not found, we
+     * don't want to throw an exception. We can't use .orElse() to create an instance
+     * of a generic class because of type erasure.
+     */
     public static <T, E> Set<T> getKeysByValue(Map<T, E> map, E value) {
         return map.entrySet()
                 .stream()

84c0c974fc1f709bc0ad533a976e02906917e282
Eric Balawejder <eric.balawejder@protonmail.com>
2021-12-18 02:56:13 -0500

    Refactored step() method and map name.

--- src/main/java/aoc/year2021/day11/Day11.java
+++ src/main/java/aoc/year2021/day11/Day11.java
@@ -19,64 +19,64 @@ public class Day11 {
 
     public static void main(String[] args) {
         final String path = "src/main/java/aoc/year2021/day11/energy-level.txt";
-        final Map<Coordinate, Integer> octopuses = readOctopuses(path);
-        System.out.println(generateSteps(octopuses, 100));
-        System.out.println(findSynchronizedMap(octopuses));
+        final Map<Coordinate, Integer> octopusesByEnergy = readOctopusesByEnergy(path);
+        System.out.println(generateSteps(octopusesByEnergy, 100));
+        System.out.println(findSynchronizedMap(octopusesByEnergy));
     }
 
-    private static int generateSteps(Map<Coordinate, Integer> octopuses, int steps) {
-        final Map<Coordinate, Integer> energyLevels = new HashMap<>(octopuses);
+    private static int generateSteps(Map<Coordinate, Integer> octopusesByEnergy, int steps) {
+        final Map<Coordinate, Integer> energyLevels = new HashMap<>(octopusesByEnergy);
         return IntStream.range(0, steps)
                 .map(i -> step(energyLevels))
-                .sum();
+                .reduce(0, Integer::sum);
     }
 
-    private static int findSynchronizedMap(Map<Coordinate, Integer> octopuses) {
-        final Map<Coordinate, Integer> energyLevels = new HashMap<>(octopuses);
+    private static int findSynchronizedMap(Map<Coordinate, Integer> octopusesByEnergy) {
+        final Map<Coordinate, Integer> energyLevels = new HashMap<>(octopusesByEnergy);
         return (int) IntStream.iterate(0, i -> i + 1)
                 .peek(i -> step(energyLevels))
                 .takeWhile(i -> !isSynchronized(energyLevels))
                 .count() + 1;
     }
 
-    private static int step(Map<Coordinate, Integer> octopuses) {
-        final Set<Coordinate> flashers = new HashSet<>();
-        final Queue<Coordinate> toIncrease = new LinkedList<>(octopuses.keySet());
-        while (!toIncrease.isEmpty()) {
-            final Coordinate top = toIncrease.poll();
-            if (!flashers.contains(top)) {
-                final Integer value = octopuses.compute(top, (k, v) -> v == null ? null : v + 1);
-                if (value > 9) {
-                    flashers.add(top);
-                    top.adjacents()
+    private static int step(Map<Coordinate, Integer> octopusesByEnergy) {
+        final Set<Coordinate> flashed = new HashSet<>();
+        final Queue<Coordinate> coordinates = new LinkedList<>(octopusesByEnergy.keySet());
+        while (!coordinates.isEmpty()) {
+            final Coordinate coordinate = coordinates.poll();
+            if (!flashed.contains(coordinate)) {
+                final Integer energyLevel = octopusesByEnergy.computeIfPresent(coordinate, (k, v) -> v + 1);
+                if (energyLevel != null && energyLevel > 9) {
+                    flashed.add(coordinate);
+                    coordinate.adjacents()
                             .stream()
-                            .filter(c -> octopuses.containsKey(c) && !flashers.contains(c))
-                            .forEach(toIncrease::add);
+                            .filter(c -> octopusesByEnergy.containsKey(c) && !flashed.contains(c))
+                            .forEach(coordinates::add);
                 }
             }
         }
-        flashers.forEach(c -> octopuses.put(c, 0));
-        return flashers.size();
+        flashed.forEach(coordinate -> octopusesByEnergy.put(coordinate, 0));
+        return flashed.size();
     }
 
-    private static boolean isSynchronized(Map<Coordinate, Integer> octopuses) {
-        return octopuses.values()
+    private static boolean isSynchronized(Map<Coordinate, Integer> octopusesByEnergy) {
+        return octopusesByEnergy.values()
                 .stream()
                 .allMatch(i -> i == 0);
     }
 
-    private static Map<Coordinate, Integer> readOctopuses(String path) {
+    private static Map<Coordinate, Integer> readOctopusesByEnergy(String path) {
         try (Stream<String> stream = Files.lines(Paths.get(path))) {
             final List<String> lines = stream.toList();
-            final Map<Coordinate, Integer> octopuses = new LinkedHashMap<>();
+            final Map<Coordinate, Integer> octopusesByEnergy = new LinkedHashMap<>();
             for (int y = 0; y < lines.size(); y++) {
                 final String line = lines.get(y);
                 for (int x = 0; x < line.length(); x++) {
                     final int number = Integer.parseInt(String.valueOf(line.charAt(x)));
-                    octopuses.put(new Coordinate(x, y), number);
+                    octopusesByEnergy.put(new Coordinate(x, y), number);
                 }
             }
-            return Collections.unmodifiableMap(octopuses);
+            return Collections.unmodifiableMap(octopusesByEnergy);
         } catch (IOException e) {
             e.printStackTrace();
             return Collections.emptyMap();

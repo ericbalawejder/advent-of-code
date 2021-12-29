a57ade06a05b97d0588df0acfbb8aa813464edc1
Eric Balawejder <eric.balawejder@protonmail.com>
2021-12-29 02:20:23 -0500

    Day14

--- src/main/java/aoc/year2021/day14/ExtendedPolymerization.java
+++ src/main/java/aoc/year2021/day14/ExtendedPolymerization.java
@@ -4,79 +4,87 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class ExtendedPolymerization {
 
-    /**
-     * Use TreeMap to sort by value -> occurrences of letters
-     * use hashmap, sort values, get min and max key
-     */
     public static void main(String[] args) throws IOException {
-        //final String path = "src/main/java/aoc/year2021/day14/pair-insertion-rules.txt";
-        final String path = "src/main/java/aoc/year2021/day14/test.txt";
+        final String path = "src/main/java/aoc/year2021/day14/pair-insertion-rules.txt";
         final Optional<String> polymerTemplate = getPolymerTemplate(path);
         final Map<String, String> pairInsertionRules = getPairInsertionRules(path);
-
-        final String polymer = generatePolymer(pairInsertionRules, polymerTemplate.orElseThrow(), 40);
-        System.out.println(polymer.length());
-        System.out.println(frequency(polymer));
-        System.out.println(polymerMath(polymer));
+        final long maxMinusMin1 = mostMinusLeast(pairInsertionRules, polymerTemplate.orElseThrow(), 10);
+        final long maxMinusMin2 = mostMinusLeast(pairInsertionRules, polymerTemplate.orElseThrow(), 40);
+        System.out.println(maxMinusMin1);
+        System.out.println(maxMinusMin2);
     }
 
-    static long polymerMath(String polymer) {
-        final List<Map.Entry<Character, Long>> characterCount = frequency(polymer);
-        final long max = characterCount.get(characterCount.size() - 1).getValue();
-        final long min = characterCount.get(0).getValue();
-        return max - min;
+    static long mostMinusLeast(Map<String, String> pairInsertionRules,
+                               String polymerTemplate,
+                               int steps) {
+        final Map<Character, Long> characterCount =
+                countPolymerCharacters(pairInsertionRules, polymerTemplate, steps);
+
+        final List<Map.Entry<Character, Long>> sorted = characterCount.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .toList();
+
+        return sorted.get(sorted.size() - 1).getValue() - sorted.get(0).getValue();
     }
 
-    static List<Map.Entry<Character, Long>> frequency(String polymer) {
-        final Map<Character, Long> characterCount = polymer.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(Function.identity(),
-                        Collectors.counting()));
-
-        final Comparator<Map.Entry<Character, Long>> byValue =
-                Map.Entry.comparingByValue();
-
-        // use TreeSet
+    private static Map<Character, Long> countPolymerCharacters(Map<String, String> pairInsertionRules,
+                                                               String template,
+                                                               int steps) {
+        final Map<String, Long> pairCount = generatePolymer(pairInsertionRules, template, steps);
+        final Map<Character, Long> characterCount = new HashMap<>();
+        characterCount.compute(template.charAt(0), (k, v) -> v == null ? 1L : v + 1L);
+        characterCount.compute(template.charAt(template.length() - 1), (k, v) -> v == null ? 1L : v + 1L);
+        pairCount.forEach((key, value) -> {
+            characterCount.compute(key.charAt(0), (k, v) -> v == null ? value : v + value);
+            characterCount.compute(key.charAt(1), (k, v) -> v == null ? value : v + value);
+        });
         return characterCount.entrySet()
                 .stream()
-                .sorted(byValue)
-                .toList();
+                .map(e -> Map.entry(e.getKey(), e.getValue() / 2))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    private static String generatePolymer(Map<String, String> pairInsertionRules,
-                                  String polymerTemplate,
-                                  int steps) {
-        String polymer = polymerTemplate;
+    private static Map<String, Long> generatePolymer(Map<String, String> pairInsertionRules,
+                                                     String polymerTemplate,
+                                                     int steps) {
+        final Map<String, Long> polymerTemplatePairCount = polymerTemplatePairCount(polymerTemplate);
+        Map<String, Long> pairCount = new HashMap<>(polymerTemplatePairCount);
         for (int i = 0; i < steps; i++) {
-            polymer = polymerStep(pairInsertionRules, polymer);
+            pairCount = step(pairCount, pairInsertionRules);
         }
-        return polymer;
+        return Map.copyOf(pairCount);
     }
 
-    private static String polymerStep(Map<String, String> pairInsertionRules, String polymerTemplate) {
-        final StringBuilder polymer = new StringBuilder(polymerTemplate.substring(0, 1));
-        for (int i = 0; i <= polymerTemplate.length() - 2; i++) {
-            final String pair = polymerTemplate.substring(i, i + 2);
-            final String[] s = pair.split("");
-            if (pairInsertionRules.containsKey(pair)) {
-                final String value = pairInsertionRules.get(pair);
-                polymer.append(value);
-                polymer.append(s[1]);
-            } else {
-                polymer.append(s[1]);
-            }
+    private static Map<String, Long> step(Map<String, Long> pairCount, Map<String, String> rules) {
+        final Map<String, Long> polymerPairCount = new HashMap<>();
+        for (String key : pairCount.keySet()) {
+            final String value = rules.get(key);
+            final String p1 = key.charAt(0) + value;
+            final String p2 = value + key.charAt(1);
+            final long count = pairCount.getOrDefault(key, 0L);
+            polymerPairCount.compute(p1, (k, v) -> v == null ? count : v + count);
+            polymerPairCount.compute(p2, (k, v) -> v == null ? count : v + count);
         }
-        return polymer.toString();
+        return Map.copyOf(polymerPairCount);
+    }
+
+    private static Map<String, Long> polymerTemplatePairCount(String polymerTemplate) {
+        return IntStream.range(0, polymerTemplate.length() - 1)
+                .mapToObj(i -> polymerTemplate.substring(i, i + 2))
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
     }
 
     private static Map<String, String> getPairInsertionRules(String path) {
